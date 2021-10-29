f5f37e1eb7ccb401381ea0c5cf676ec29c269a4d
paul.lin <paul.lin@adlinktech.com>
2021-10-29 16:37:48 +0800

    add run.sh

---
+++ src/demo/part-preparation/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LB='\033[1;33m'
+NC='\033[0m' # No Color
+
+# echo message with color
+message_out(){
+    echo -e "${LB}=> $1${NC}"
+}
+
+ModelPruned=${1:-no};
+
+message_out "Start running part preparation demo..."
+
+if [ $ModelPruned == "no" ]
+then
+    message_out "[without pruned model] run first demo video"
+    gst-launch-1.0 filesrc location=material-preparation.avi ! decodebin ! nvvideoconvert ! videoscale ! video/x-raw, width=960, height=540 ! videoconvert ! adrt model=model/dssd_resnet18_epoch_3400_fp32.engine device=0 ! adtrans_ssd max-count=50 label= model/SHMC-label.txt threshold=0.2 ! partpreparation alert-type=order-incorrect ! voice_alert alert-type=order-incorrect speech-content="order-incorrect" ! videoconvert ! xvimagesink &
+    
+    message_out "[without pruned model] run second demo video"
+    gst-launch-1.0 filesrc location=disassembly.avi ! decodebin ! nvvideoconvert ! videoscale ! video/x-raw, width=960, height=540 ! videoconvert ! adrt model=model/dssd_resnet18_epoch_3400_fp32.engine device=0 ! adtrans_ssd max-count=50 label= model/SHMC-label.txt threshold=0.2 ! partpreparation alert-type=order-incorrect ! voice_alert alert-type=order-incorrect speech-content="order-incorrect" ! videoconvert ! xvimagesink &
+    
+    message_out "[without pruned model] run third demo video"
+    gst-launch-1.0 filesrc location=order-incorrect.avi ! decodebin ! nvvideoconvert ! videoscale ! video/x-raw, width=960, height=540 ! videoconvert ! adrt model=model/dssd_resnet18_epoch_3400_fp32.engine device=0 ! adtrans_ssd max-count=50 label= model/SHMC-label.txt threshold=0.2 ! partpreparation alert-type=order-incorrect ! voice_alert alert-type=order-incorrect speech-content="order-incorrect" ! videoconvert ! xvimagesink &
+    
+elif [ $ModelPruned == "yes" ]
+then
+    message_out "[pruned model] run first demo video"
+    gst-launch-1.0 filesrc location=material-preparation.avi ! decodebin ! nvvideoconvert ! videoscale ! video/x-raw, width=960, height=540 ! videoconvert ! adrt model=model/dssd_resnet18_epoch_810_fp32.engine device=0 ! adtrans_ssd max-count=50 label= model/SHMC-label.txt threshold=0.2 ! partpreparation alert-type=order-incorrect ! voice_alert alert-type=order-incorrect speech-content="order-incorrect" ! videoconvert ! xvimagesink &
+    
+    message_out "[pruned model] run second demo video"
+    gst-launch-1.0 filesrc location=disassembly.avi ! decodebin ! nvvideoconvert ! videoscale ! video/x-raw, width=960, height=540 ! videoconvert ! adrt model=model/dssd_resnet18_epoch_810_fp32.engine device=0 ! adtrans_ssd max-count=50 label= model/SHMC-label.txt threshold=0.2 ! partpreparation alert-type=order-incorrect ! voice_alert alert-type=order-incorrect speech-content="order-incorrect" ! videoconvert ! xvimagesink &
+    
+    message_out "[pruned model] run third demo video"
+    gst-launch-1.0 filesrc location=order-incorrect.avi ! decodebin ! nvvideoconvert ! videoscale ! video/x-raw, width=960, height=540 ! videoconvert ! adrt model=model/dssd_resnet18_epoch_810_fp32.engine device=0 ! adtrans_ssd max-count=50 label= model/SHMC-label.txt threshold=0.2 ! partpreparation alert-type=order-incorrect ! voice_alert alert-type=order-incorrect speech-content="order-incorrect" ! videoconvert ! xvimagesink &
+
+else
+    message_out "input invalid parameter."
+fi
+
