db24d85a6c2c9ed9df8a68aef60f1629aa4c45e1
paul.lin <paul.lin@adlinktech.com>
2021-11-02 17:20:39 +0800

    modify install.sh

--- src/demo/part-assembly/install.sh
+++ src/demo/part-assembly/install.sh
@@ -337,8 +337,8 @@ if [[ " ${arch_jetson_name[*]} " =~ " ${jetson_name} " ]]; then
         
         # unzip it, then delete the zip file
         sudo apt-get -y install unzip
-        unzip -o tao-converter-jp4.5.zip
-        rm tao-converter-jp4.5.zip
+        unzip -o tao-converter-jp4.5
+        rm tao-converter-jp4.5
         cd jp4.5
         sudo chmod +x tao-converter
         
