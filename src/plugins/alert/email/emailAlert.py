3fedf3fe4c851ecb92e9249791be60f77985cd57
IST-EVA-Support <83396962+IST-EVA-Support@users.noreply.github.com>
2023-10-11 17:26:33 +0800

    Update emailAlert.py
    alert mail application code changed.

--- src/plugins/alert/email/emailAlert.py
+++ src/plugins/alert/email/emailAlert.py
@@ -159,7 +159,7 @@ class EmailAlert(Gst.Element):
                             #print("In python element, meta information = ", metaString)
                             self.send_time = time.time()
                             if self.senderPwd == "":
-                                self.senderPwd = "vbcsekwzsxxbgymh"
+                                self.senderPwd = "xjdtukuldxunugdl"
 
                             if not self.attachEventImage:
                                 mail_thread = threading.Thread(target=send_mail_txt,
