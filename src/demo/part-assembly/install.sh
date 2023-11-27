b888f45b7f455a7413bf96a2c39b84511e55d731
keabcya <48827149+keabcya@users.noreply.github.com>
2023-11-27 16:39:57 +0800

    Update install.sh

--- src/demo/part-assembly/install.sh
+++ src/demo/part-assembly/install.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+modelPruned="no_pruned"
+
+# GPU architecture checker
+gpuArchChecker="jetson"
+
+# echo "OPTIND starts at $OPTIND"
+while getopts "m:g:?" optname
+  do
+    case "$optname" in
+      "m")
+#         echo "Option $optname is specified"
+        modelPruned=$OPTARG
+        ;;
+      "g")
+#         echo "Option $optname has value $OPTARG"
+        gpuArchChecker=$OPTARG
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        ;;
+    esac
+  done
+
+
+# echo "modelPruned = ${modelPruned}"
+echo "gpuArchChecker = ${gpuArchChecker}"
+
 LB='\033[1;33m'
 NC='\033[0m' # No Color
 
@@ -8,32 +35,107 @@ message_out(){
     echo -e "${LB}=> $1${NC}"
 }
 
-metadata_version=${1:-2};
+# Get current path
+current_path=`pwd`
 
-message_out "Start running part assembly demo..."
 
-if [ $metadata_version == "2" ]
+# Check is in ADLINK EVA container
+sudoString="sudo"
+if [ -e "/entrypoint.sh" ]
 then
-    message_out "[metadata version 2] run first demo video"
-    gst-launch-1.0 filesrc location=normal-assembly.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! nvvidconv ! videoconvert ! adrt model=yolov4-608.engine scale=0.0039 rgbconv=True mean="0 0 0" engine-id=adrt query="//" ! adtrans_yolo topology=1 label=SHMC-label.txt class-num=10 blob-size="76,38,19" mask="(0,1,2),(3,4,5),(6,7,8)" anchor="(12,16),(19,36),(40,28),(36,75),(76,55),(72,146),(142,110),(192,243),(459,401)" use-sigmoid=True input-width=608 input-height=608 ! partassembly alert-type=idling query="//adrt" ! voice_alert alert-type=idling speech-content="idling" query="//adrt" ! videoconvert ! xvimagesink & wait
-    
-    message_out "[metadata version 2] run second demo video"
-    gst-launch-1.0 filesrc location=normal-assembly-quick.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! nvvidconv ! videoconvert ! adrt model=yolov4-608.engine scale=0.0039 rgbconv=True mean="0 0 0" engine-id=adrt query="//" ! adtrans_yolo topology=1 label=SHMC-label.txt class-num=10 blob-size="76,38,19" mask="(0,1,2),(3,4,5),(6,7,8)" anchor="(12,16),(19,36),(40,28),(36,75),(76,55),(72,146),(142,110),(192,243),(459,401)" use-sigmoid=True input-width=608 input-height=608 ! partassembly alert-type=idling query="//adrt" ! voice_alert alert-type=idling speech-content="idling" query="//adrt" ! videoconvert ! xvimagesink & wait 
-    
-    message_out "[metadata version 2] run third demo video"
-    gst-launch-1.0 filesrc location=long-time-interval.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! nvvidconv ! videoconvert ! adrt model=yolov4-608.engine scale=0.0039 rgbconv=True mean="0 0 0" engine-id=adrt query="//" ! adtrans_yolo topology=1 label=SHMC-label.txt class-num=10 blob-size="76,38,19" mask="(0,1,2),(3,4,5),(6,7,8)" anchor="(12,16),(19,36),(40,28),(36,75),(76,55),(72,146),(142,110),(192,243),(459,401)" use-sigmoid=True input-width=608 input-height=608 ! partassembly alert-type=idling query="//adrt" ! voice_alert alert-type=idling speech-content="idling" query="//adrt" ! videoconvert ! xvimagesink & wait
-    
-elif [ $metadata_version == "1" ]
-then
-    message_out "[metadata version 1] run first demo video"
-    gst-launch-1.0 filesrc location=normal-assembly.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! nvvidconv ! videoconvert ! adrt model=yolov4-608.engine scale=0.0039 rgbconv=True mean="0 0 0" ! adtrans_yolo topology=1 label=SHMC-label.txt class-num=10 blob-size="76,38,19" mask="(0,1,2),(3,4,5),(6,7,8)" anchor="(12,16),(19,36),(40,28),(36,75),(76,55),(72,146),(142,110),(192,243),(459,401)" use-sigmoid=True input-width=608 input-height=608 ! partassembly alert-type=idling ! voice_alert alert-type=idling speech-content="idling" ! videoconvert ! xvimagesink & wait
-    
-    message_out "[metadata version 1] run second demo video"
-    gst-launch-1.0 filesrc location=normal-assembly-quick.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! nvvidconv ! videoconvert ! adrt model=yolov4-608.engine scale=0.0039 rgbconv=True mean="0 0 0" ! adtrans_yolo topology=1 label=SHMC-label.txt class-num=10 blob-size="76,38,19" mask="(0,1,2),(3,4,5),(6,7,8)" anchor="(12,16),(19,36),(40,28),(36,75),(76,55),(72,146),(142,110),(192,243),(459,401)" use-sigmoid=True input-width=608 input-height=608 ! partassembly alert-type=idling ! voice_alert alert-type=idling speech-content="idling" ! videoconvert ! xvimagesink & wait
-    
-    message_out "[metadata version 1] run third demo video"
-    gst-launch-1.0 filesrc location=long-time-interval.mp4 ! qtdemux ! h264parse ! nvv4l2decoder ! nvvidconv ! videoconvert ! adrt model=yolov4-608.engine scale=0.0039 rgbconv=True mean="0 0 0" ! adtrans_yolo topology=1 label=SHMC-label.txt class-num=10 blob-size="76,38,19" mask="(0,1,2),(3,4,5),(6,7,8)" anchor="(12,16),(19,36),(40,28),(36,75),(76,55),(72,146),(142,110),(192,243),(459,401)" use-sigmoid=True input-width=608 input-height=608 ! partassembly alert-type=idling ! voice_alert alert-type=idling speech-content="idling" ! videoconvert ! xvimagesink & wait
-
-else
-    message_out "input invalid parameter."
+    sudoString=" "
 fi
+message_out "sudo String = ${sudoString}"
+
+
+message_out "Start installing..."
+
+# build required plugin
+message_out "Building assembly plugin..."
+${sudoString} apt update
+${sudoString} apt -y install libgstreamer-plugins-base1.0-dev
+${sudoString} apt -y install libopencv-dev python3-opencv
+${sudoString} apt -y install wget
+${sudoString} apt -y install libnvinfer-bin
+pip3 install -r requirements.txt
+../../plugins/assembly/assembly-build.sh
+
+
+# ####
+# download video
+if [ -e "long-time-interval.mp4" ]
+then
+    message_out "Demo video exists, skip."
+else
+    message_out "Start download demo video [long-time-interval.mp4]..."
+    wget https://sftp.adlinktech.com/image/EVA/EVA_Show-Case/showcase5/long-time-interval.mp4
+fi
+if [ -e "normal-assembly.mp4" ]
+then
+    message_out "Demo video exists, skip."
+else
+    message_out "Start download demo video [normal-assembly.mp4]..."
+    wget https://sftp.adlinktech.com/image/EVA/EVA_Show-Case/showcase5/normal-assembly.mp4
+fi
+if [ -e "normal-assembly-quick.mp4" ]
+then
+    message_out "Demo video exists, skip."
+else
+    message_out "Start download demo video [normal-assembly-quick.mp4]..."
+    wget https://sftp.adlinktech.com/image/EVA/EVA_Show-Case/showcase5/normal-assembly-quick.mp4
+fi
+if [ -e "prepare-assembly-1.mp4" ]
+then
+    message_out "Demo video exists, skip."
+else
+    message_out "Start download demo video [prepare-assembly-1.mp4]..."
+    wget https://sftp.adlinktech.com/image/EVA/EVA_Show-Case/showcase5/prepare-assembly-1.mp4
+fi
+if [ -e "prepare-assembly-2.mp4" ]
+then
+    message_out "Demo video exists, skip."
+else
+    message_out "Start download demo video [prepare-assembly-2.mp4]..."
+    wget https://sftp.adlinktech.com/image/EVA/EVA_Show-Case/showcase5/prepare-assembly-2.mp4
+fi
+if [ -e "prepare-assembly-idle.mp4" ]
+then
+    message_out "Demo video exists, skip."
+else
+    message_out "Start download demo video [prepare-assembly-idle.mp4]..."
+    wget https://sftp.adlinktech.com/image/EVA/EVA_Show-Case/showcase5/prepare-assembly-idle.mp4
+fi
+
+
+# download model and label zip file
+if [ -e "yolov4-tiny-608.onnx" ]
+then
+    message_out "onnx model file exists."
+else    
+    message_out "Start download model..."
+    wget https://sftp.adlinktech.com/image/EVA/EVA_Show-Case/showcase5/model-yolov4.zip
+
+    # unzip it, then delete the zip file
+    ${sudoString} apt-get -y install unzip
+    unzip model-yolov4.zip
+    rm model-yolov4.zip
+fi
+
+message_out "Start convert onnx model to tensorrt..."    
+../../scripts/optimize_yolov4.sh yolov4-608.onnx yolov4-608.engine
+message_out "Convert Done."
+
+
+# python plugin
+cd $current_path
+message_out "Deploy alert plugin..."
+../../plugins/alert/email/emailAlert-build.sh
+../../plugins/alert/voice/voiceAlert-build.sh
+
+message_out "Install related python package"
+pip3 install -r requirements.txt
+${sudoString} apt -y install gstreamer1.0-libav
+${sudoString} apt-get -y install espeak
+rm ~/.cache/gstreamer-1.0/regi*
+
+message_out "Installation completed, you could run this demo by execute ./run.sh"
